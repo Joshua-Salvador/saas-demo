c33f8364be5c42ea60f721f92a94b3410c8c4ea4
Joshua-Salvador <424joshuae.b.s@gmail.com>
2021-03-19 20:40:18 +0800

    Create Project Form

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAtBSRFnl17D7rrZ3WpEuVV7b2MwjAGW38",
+  authDomain: "yektopro-347fb.firebaseapp.com",
+  projectId: "yektopro-347fb",
+  storageBucket: "yektopro-347fb.appspot.com",
+  messagingSenderId: "996014756304",
+  appId: "1:996014756304:web:c0d8782310fce1dadce9d5",
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
