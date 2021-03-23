2f9f73e58ec2920cb36aff4d457603fde5091a70
Joshua-Salvador <424joshuae.b.s@gmail.com>
2021-03-23 18:09:41 +0800

    Routing

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Asset from "./containers/Forms/create/Asset";
 import Organization from "./containers/Forms/create/Organization";
 import User from "./containers/Forms/create/User";
 import AuthProvider from "./contexts/AuthProvider";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
-        <Login />
-      
-        <Organization />
-        <User />
-        <Asset />
-      </AuthProvider>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Organization} />
+            <Route exact path="/create-user" component={User} />
+            <Route exact path="/create-asset" component={Asset} />
+          </Switch>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
